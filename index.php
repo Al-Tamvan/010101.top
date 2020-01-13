1cc8959a4543d8de93a34e18d416dc7997ddfe75
Al-Tamvan <59823338+Al-Tamvan@users.noreply.github.com>
2020-01-13 16:39:10 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<meta charset="Utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<script>
+		alert('CIDUK GUA MIMIN TAMVAN');
+		</script>
+		<title>Root@~: Syntax_Error</title>
+		<style>
+			body{
+				background-color:black;
+			}
+			.text pre{
+				color:yellow;
+				font-size: 15px;
+			}
+		</style>
+	</head>
+	<body>
+	<center><font color="white" size="10px">Infected by Syntax_Error</font></center>
+	<br>
+<br>
+<br>
+	<div class="text">
+		<center><pre>
+			Kamu jangan pergi,Hatiku masih mencitaimu
+		</pre><center>
+	</div>
+	<marquee><font color="red" size="5px">INDONESIA<font color="white">HACKER RULEZ</font></marquee>
+<br>
+<br>
+<br>
+	<br>
+<br>
+<br>
+	<br>
+<br>
+<br>
+	<br>
+<br>
+<br>
+	<br>
+<br>
+<br>
+	<br>
+<br>
+<br>
+	<br>
+<br>
+<br>
+<center>
+<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="788.54" height="443" type="text/html" src="https://www.youtube.com/embed/idPf-_2Lkwg?autoplay=1&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"></center>
+</body>
+</html>
